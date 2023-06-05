6a3c544e7f61101e1da61a6fb812d282920889ef
Jayesh Vegda <130431390+JayeshVegda@users.noreply.github.com>
2023-06-05 10:11:41 +0530

    Project 1

--- 1. Color Flipper/jquary.js
+++ 1. Color Flipper/jquary.js
@@ -1,3 +1,8 @@
-function mytry(){
-    document.getElementById("red").style.color = "red";
+function mytry(id){
+    let my_id = id
+    let a = document.getElementById(id)
+    let a2 = document.getElementById('mytext')
+    console.log(a2)
+    document.body.style.backgroundColor = id
+    a2.innerText = `${id} Color`.toUpperCase()
 }
