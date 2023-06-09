56311a617b59b3d51a425f3f9cf8a6915b31d3ce
Jayesh Vegda <130431390+JayeshVegda@users.noreply.github.com>
2023-06-09 19:39:02 +0530

    try

--- 3. Rock Paper Secicor/script.js
+++ 3. Rock Paper Secicor/script.js
@@ -1,13 +1,37 @@
+let game = ["rock", "paper", "sci"]
+let whoname = prompt("Enter Your Name Buddy")
+
+function hide(clickedItemId){
+//   var elements = document.getElementsByClassName('bb');
+//   console.log(element)
+//   // Loop through each element
+//   for (var i = 0; i < elements.length; i++) {
+//     var element = elements[i];
+    
+//     // Check if the element is the clicked item
+//     if (element.id === clickedItemId) {
+//       // Disable the clicked item
+//       element.style.pointerEvents = 'none';
+//     } else {
+//       // Disable the unclicked items
+//       element.style.opacity = '0.5'; // You can customize the style to indicate the disabled state
+//       element.style.pointerEvents = 'none';
+//     }
+//   }
+  my_game(clickedItemId)
+}
+
 
 function my_game(id){
-    let game = ["rock", "paper", "sci"]
+    
     let set_win = document.getElementById("output")
-
-    let point = document.getElementById("point")
-    let my_point = document.getElementById("point").innerText
-    let real_point = Number.parseInt(my_point)
-
     var random = game[(Math.random() * game.length) | 0]
+    let point = document.getElementById("point")
+    let my_name = document.getElementById("name")
+    my_name.innerText = whoname
+    let my_point = document.getElementById("point").innerText
+    var real_point = Number.parseInt(my_point)
+
     let choice = document.getElementById(id).id
 
     if(choice == "rock" && random == "rock"){
@@ -17,7 +41,7 @@ function my_game(id){
     else if(choice == "rock" && random == "paper"){
         console.log("You Lose Buddy")
         set_win.innerText = "Result : Lose"
-        point.innerText = 0
+        playagain()
     }
     else if(choice == "rock" && random == "sci"){
         console.log("You win Buddy")
@@ -38,7 +62,8 @@ function my_game(id){
     else if(choice == "paper" && random == "sci"){
         console.log("You Lose Buddy")
         set_win.innerText = "Result : Lose"
-        point.innerText = 0
+        playagain()
+
     }
     
     
@@ -54,12 +79,19 @@ function my_game(id){
     else if(choice == "sci" && random == "rock"){
         console.log("You Lose Buddy")
         set_win.innerText = "Result : Lose"
-        point.innerText = 0
+        playagain()
+        
     }
     else{
         console.log("Enter Valid input")
     }
     console.log(choice)
+}
+
+function playagain(){
+    alert(`Your Score is ${real_point}`)
+    alert("Do You wanna play Again ?")
+    point.innerText = 0
 }
 
 // let choice = prompt("enter the name of value : ")
